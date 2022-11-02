f61cb53ff4d020ea322b0e6410752dd672ced0ef
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-11-02 22:06:02 +0300

    Add possibility to change alpha channel

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 log.addHandler(logging.FileHandler('image.log'))
 
+
+def nothing(x):
+    pass
+
+
 image = cv2.imread('image.jpg')
 cv2.namedWindow('image')
+cv2.createTrackbar('alpha', 'image', 0, 255, nothing)
 
 overlay = image.copy()
 output = image.copy()
@@ -29,17 +35,14 @@ def draw(event, x, y, flags, param):
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing:
             # cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            cv2.rectangle(overlay, (start_x, start_y), (x, y), (0, 200, 0), -1)  # A filled rectangle
-            alpha = 0.4  # Transparency factor.
+            cv2.rectangle(overlay, (start_x, start_y), (x, y), (200, 65, 65), -1)  # A filled rectangle
+            alpha = cv2.getTrackbarPos('alpha',
+                                       'image') / 255  # Transparency factor. Get from trackbar and map to [0,1]
             # Following line overlays transparent rectangle over the image
             cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, output)
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        # cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-        cv2.rectangle(overlay, (start_x, start_y), (x, y), (0, 200, 0), -1)  # A filled rectangle
-        alpha = 0.4  # Transparency factor.
-        # Following line overlays transparent rectangle over the image
-        cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, output)
+        cv2.rectangle(overlay, (start_x, start_y), (x, y), (200, 65, 65), -1)  # A filled rectangle
 
 
 def mouse_click(event, x, y,
@@ -57,7 +60,9 @@ while True:
     cv2.imshow('image', output)
     key = cv2.waitKey(1)
     log.info(key)
-    #print(key)
+    # print(key)
+    alpha = cv2.getTrackbarPos('alpha', 'image') / 255
+    cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, output)
 
     if key == 32:  # space
         #  delete all drawings
@@ -68,53 +73,6 @@ while True:
 
 cv2.destroyAllWindows()  # destroys the window showing image
 
-# import numpy as np
-# import cv2 as cv
-#
-# drawing = False  # true if mouse is pressed
-# mode = True  # if True, draw rectangle. Press 'm' to toggle to curve
-# ix, iy = -1, -1
-#
-#
-# # mouse callback function
-# def draw_circle(event, x, y, flags, param):
-#     global ix, iy, drawing, mode, img
-#     if event == cv.EVENT_LBUTTONDOWN:
-#         drawing = True
-#         ix, iy = x, y
-#     elif event == cv.EVENT_MOUSEMOVE:
-#         if drawing == True:
-#             if mode == True:
-#                 # cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-#                 cv.rectangle(overlay, (ix, iy), (x, y), (0, 200, 0), -1)  # A filled rectangle
-#                 alpha = 0.4  # Transparency factor.
-#                 # Following line overlays transparent rectangle over the image
-#                 cv.addWeighted(overlay, alpha, img, 1 - alpha, 0, output)
-#             else:
-#                 cv.circle(img, (x, y), 5, (0, 0, 255), -1)
-#     elif event == cv.EVENT_LBUTTONUP:
-#         drawing = False
-#         if mode == True:
-#             # cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-#             cv.rectangle(overlay, (ix, iy), (x, y), (0, 200, 0), -1)  # A filled rectangle
-#             alpha = 0.4  # Transparency factor.
-#             # Following line overlays transparent rectangle over the image
-#             cv.addWeighted(overlay, alpha, img, 1 - alpha, 0, output)
-#         else:
-#             cv.circle(img, (x, y), 5, (0, 0, 255), -1)
-#
-#
-# img = cv.imread('image.jpg')
-# overlay = img.copy()
-# output = img.copy()
-# # img = np.zeros((512,512,3), np.uint8)
-# cv.namedWindow('image')
-# cv.setMouseCallback('image', draw_circle)
-# while (1):
-#     cv.imshow('image', output)
-#     k = cv.waitKey(1) & 0xFF
-#     if k == ord('m'):
-#         mode = not mode
-#     elif k == 27:
-#         break
-# cv.destroyAllWindows()
+# TODO:
+# сделать возможность рисовать только один rect и сохранять его координате, если не нравится -> перерисовать, иначе
+# добавить кнопку, которая отвечает за обрезание фотографии
